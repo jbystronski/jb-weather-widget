fb1dc0623d0403c1129c804dd0b2b528ed2de454
Jakub Bystroński <jbstr85@gmail.com>
2023-01-12 01:33:17 +0100

    misc

--- src/utils/autodectLocation.js
+++
@@ -1,8 +0,0 @@
-export function autodetectLocation() {
-  return fetch("http://ip-api.com/json").then((result) =>
-    result
-      .json()
-      .then((data) => console.log(data))
-      .catch(console.error)
-  );
-}
